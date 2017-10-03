583edc2c7891a690c479bddb6238501e76f5a3cb
aregnazarian <areg.nazarian1@marist.edu>
2017-10-02 20:30:40 -0400

    commit

--- lab5-server.js
+++ lab5-server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
+app.use(express.static('./public'))
 
-
-app.listen(1337, function () {
-  console.log('Example app listening on port 1337!')
-})
 
 
 app.get('/',   function   (req,   res)   {
 res.sendFile('views/index.html',   {root:   __dirname   }) })
 
 
-app.use(express.static('public'))
+
+
+
+
+app.listen(1337, function () {
+  console.log('Example app listening on port 1337!')
+})
